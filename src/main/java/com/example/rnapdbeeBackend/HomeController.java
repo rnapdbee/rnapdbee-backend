67930e86b14d8acf55900e2feaf4263e09a546a7
sniochup <snioszekpawel@gmail.com>
2022-04-06 19:25:31 +0200

    feat: backend Spring repository init

---
+++ src/main/java/com/example/rnapdbeeBackend/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.rnapdbeeBackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "Rnapdbee backend home";
+    }
+}
---
+++ src/test/java/com/example/rnapdbeeBackend/RnapdbeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rnapdbeeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnapdbeeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
