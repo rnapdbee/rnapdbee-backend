5ead0230dc46846c6bcd3fccdc978f99058044cf
sniochup <snioszekpawel@gmail.com>
2022-04-06 19:25:31 +0200

    feat: backend Spring repository init

---
+++ src/main/java/com/example/rnapdbeeBackend/RnapdbeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rnapdbeeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnapdbeeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RnapdbeeBackendApplication.class, args);
+    }
+
+}
