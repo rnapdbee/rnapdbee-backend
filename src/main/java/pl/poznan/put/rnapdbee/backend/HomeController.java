a75ec74ff6bcb533a2d6705cfc819e364edca17f
Paweł Śnioszek <72894246+sniochup@users.noreply.github.com>
2022-07-11 23:30:43 +0200

    feat: ci config (#1)
    * ci: configuration
    
    * ref: rename packages
    
    * ci: docker in piplines config
    
    * ci: github actions test
    
    * ci: actions test
    
    * ci: github action config
    
    * ci: update actions versions
    
    * chore: skip test during maven build add
    
    * fix: reviews comments fixes

--- src/main/java/pl/poznan/put/rnapdbee/backend/HomeController.java
+++ src/main/java/pl/poznan/put/rnapdbee/backend/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.rnapdbeeBackend;
+package pl.poznan.put.rnapdbee.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
