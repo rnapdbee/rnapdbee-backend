daf3aa167a33f2ead70fbcb9445075d082fbb18c
sniochup <snioszekpawel@gmail.com>
2022-08-29 00:05:56 +0200

    fix: small changes

--- src/main/java/pl/poznan/put/rnapdbee/backend/controller/Backend3DController.java
+++ src/main/java/pl/poznan/put/rnapdbee/backend/controller/Backend3DController.java
@@ -2,7 +2,12 @@ package pl.poznan.put.rnapdbee.backend.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.poznan.put.rnapdbee.backend.model.Output3D;
 import pl.poznan.put.rnapdbee.backend.model.Payload3DThreeDots;
 
