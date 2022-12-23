2199904f5b65c2019b7ad71089267dfa2124a643
Paweł Śnioszek <72894246+sniochup@users.noreply.github.com>
2022-12-23 14:55:27 +0100

    feat: update svg path handle (#57)
    feat: update ConsensualVisualization pathToSVGImage field, change of params enum order

--- src/main/java/pl/poznan/put/rnapdbee/backend/shared/domain/param/VisualizationTool.java
+++ src/main/java/pl/poznan/put/rnapdbee/backend/shared/domain/param/VisualizationTool.java
@@ -5,9 +5,9 @@ package pl.poznan.put.rnapdbee.backend.shared.domain.param;
  * enum for VisualizationTools
  */
 public enum VisualizationTool {
+    RNA_PUZZLER,
     VARNA,
     PSEUDO_VIEWER,
     R_CHIE,
-    RNA_PUZZLER,
     NONE
 }
