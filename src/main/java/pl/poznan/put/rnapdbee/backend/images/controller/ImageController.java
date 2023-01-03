1eda94f85539cf7e995735c1dc571e4f0223b1a1
sniochup <snioszekpawel@gmail.com>
2023-01-03 23:26:06 +0100

    feat: add log for ImageController

--- src/main/java/pl/poznan/put/rnapdbee/backend/images/controller/ImageController.java
+++ src/main/java/pl/poznan/put/rnapdbee/backend/images/controller/ImageController.java
@@ -1,5 +1,7 @@
 package pl.poznan.put.rnapdbee.backend.images.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -17,16 +19,26 @@ import java.nio.file.Path;
 @RestController
 public class ImageController {
 
+    private final Logger logger = LoggerFactory.getLogger(ImageController.class);
+
     @Value("${spring.svg.images.directory.path}")
     private String imagesPath;
 
     @GetMapping("/image/{name}")
-    public ResponseEntity<Resource> getSvgImage(@PathVariable String name) throws IOException {
+    public ResponseEntity<Resource> getSvgImage(@PathVariable String name) {
+        logger.info(String.format("GET request for svg image with name: [%s]", name));
         String inputFile = String.format("%s/%s", imagesPath, name);
         Path path = new File(inputFile).toPath();
         FileSystemResource resource = new FileSystemResource(path);
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(Files.probeContentType(path)))
-                .body(resource);
+        try {
+            MediaType mediaType = MediaType.parseMediaType(Files.probeContentType(path));
+            return ResponseEntity
+                    .ok()
+                    .contentType(mediaType)
+                    .body(resource);
+        } catch (IOException e) {
+            logger.error(String.format("Error occurred during fetching [%s] svg image.", name), e);
+            throw new RuntimeException(e);
+        }
     }
 }
