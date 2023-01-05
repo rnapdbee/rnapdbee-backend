cae70bfec3277fb5c0cfe1cc54cc3cc9a2105904
Paweł Śnioszek <72894246+sniochup@users.noreply.github.com>
2023-01-05 07:36:33 +0100

    fix: request mapping, docker env (#63)
    * feat: rename env variable in docker-compose
    
    * fix: ImageController request mapping added in proper place

--- src/main/java/pl/poznan/put/rnapdbee/backend/images/controller/ImageController.java
+++ src/main/java/pl/poznan/put/rnapdbee/backend/images/controller/ImageController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
@@ -16,7 +17,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@RestController("/image")
+/**
+ * Controller class responsible for fetching svg images
+ */
+@RestController
+@RequestMapping("/image")
 public class ImageController {
 
     private final Logger logger = LoggerFactory.getLogger(ImageController.class);
